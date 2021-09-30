e2e40d3887fb0068947b6ce0b651e5792b1daedf
Klim Subboin <subbotin.klim@gmail.com>
2021-09-30 19:46:53 +0300

    add template engine

--- public/index.php
+++ public/index.php
@@ -3,8 +3,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -18,6 +24,13 @@ $app->get('/users', function ($request, $response) {
 
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = [
+        'id' => $args['id']
+    ];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
