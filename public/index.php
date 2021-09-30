5fa93e66a940a12dd301f42fd4114926027a86b5
Klim Subboin <subbotin.klim@gmail.com>
2021-09-30 21:24:19 +0300

    add new user form

--- public/index.php
+++ public/index.php
@@ -10,7 +10,8 @@ $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 
-$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$usersPath = __DIR__ . '/../data/users.json';
+$users = json_decode(file_get_contents($usersPath), true)['users'];
 
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
@@ -23,7 +24,7 @@ $app->get('/', function ($request, $response) {
 $app->get('/users', function ($request, $response) use ($users) {
     $str = $request->getQueryParam('str');
     $filtredUsers = array_filter($users, function($user) use ($users, $str) {
-        return empty($str) ? true : strpos($user, $str);
+        return empty($str) ? true : str_contains(strtolower($user['nickname']), $str);
     });
     $params = [
         'users' => $filtredUsers,
@@ -32,8 +33,21 @@ $app->get('/users', function ($request, $response) use ($users) {
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+$app->post('/users', function ($request, $response) use ($users, $usersPath) {
+    $userData = $request->getParsedBodyParam('user');
+    $userID = empty($users) ? 0 : $userData['id'] + 1;
+    $newUser = [
+        'nickname' => $userData['nickname'],
+        'email' => $userData['email'],
+        'id' => $userID
+    ];
+    $users[] = $newUser;
+    file_put_contents($usersPath, json_encode(['users' => $users]));
+    return $response->withRedirect('/users', 302);
+});
+
+$app->get('/users/new', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'users/new.phtml');
 });
 
 $app->get('/users/{id}', function ($request, $response, $args) {
