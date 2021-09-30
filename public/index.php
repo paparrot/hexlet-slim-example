c3e332fe6a799d72d0ddea16447f1bb753584257
Klim Subboin <subbotin.klim@gmail.com>
2021-10-01 00:34:59 +0300

    add flash message for creating new user

--- public/index.php
+++ public/index.php
@@ -5,9 +5,14 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+session_start();
+
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
 });
 
 $usersPath = __DIR__ . '/../data/users.json';
@@ -16,50 +21,55 @@ $users = json_decode(file_get_contents($usersPath), true)['users'];
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
-});
+})->setName("home");
 
 $app->get('/users', function ($request, $response) use ($users) {
     $str = $request->getQueryParam('str');
     $filtredUsers = array_filter($users, function($user) use ($users, $str) {
         return empty($str) ? true : str_contains(strtolower($user['nickname']), $str);
     });
+    $messages = $this->get("flash")->getMessages();
     $params = [
         'users' => $filtredUsers,
-        'str' => $str
+        'str' => $str,
+        'flash' => $messages
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName("users");
 
-$app->post('/users', function ($request, $response) use ($users, $usersPath) {
-    $userData = $request->getParsedBodyParam('user');
-    $userID = empty($users) ? 0 : $userData['id'] + 1;
-    $newUser = [
-        'nickname' => $userData['nickname'],
-        'email' => $userData['email'],
-        'id' => $userID
-    ];
+$app->post('/users', function ($request, $response) use ($users, $usersPath, $router) {
+    $newUser = $request->getParsedBodyParam('user');
     $users[] = $newUser;
+    $this->get('flash')->addMessage("success", "Пользователь добавлен");
     file_put_contents($usersPath, json_encode(['users' => $users]));
-    return $response->withRedirect('/users', 302);
-});
+    return $response->withRedirect($router->urlFor('users') , 302);
+})->setName("addUser");
 
-$app->get('/users/new', function ($request, $response) {
-    return $this->get('renderer')->render($response, 'users/new.phtml');
-});
+$app->get('/users/new', function ($request, $response) use ($users) {
+    $id = count($users);
+    return $this->get('renderer')->render($response, 'users/new.phtml', ['id'=> $id]);
+})->setName("userForm");
 
-$app->get('/users/{id}', function ($request, $response, $args) {
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $id = $args['id'];
+    $user = collect($users)->firstWhere('id', $id);
+    if(empty($user)) {
+        return $this->get('renderer')->render($response, 'users/404.phtml')->withStatus(404);
+    }
     $params = [
-        'id' => $args['id']
+        'user' => $user,
     ];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName("user");
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
-});
+})->setName("courses");
 
 $app->run();
