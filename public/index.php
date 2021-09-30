e3f2f613e3088bc18e6c70196e0086356f308bc2
Klim Subboin <subbotin.klim@gmail.com>
2021-09-30 19:35:58 +0300

    add route

--- public/index.php
+++ public/index.php
@@ -16,4 +16,8 @@ $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
 $app->run();
